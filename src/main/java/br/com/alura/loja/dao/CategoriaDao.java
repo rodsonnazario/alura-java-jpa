1c66e68964ede6f5fc42e7cde93bdcc2d54eca08
rodsonnazario <rodson.nazario@gmail.com>
2022-11-13 10:57:58 -0300

    ajuste de operações CRUD

--- src/main/java/br/com/alura/loja/dao/CategoriaDao.java
+++ src/main/java/br/com/alura/loja/dao/CategoriaDao.java
@@ -15,4 +15,13 @@ public class CategoriaDao {
 	public void cadastrar(Categoria categoria) {
 		this.em.persist(categoria);
 	}
+
+	public void atualizar(Categoria categoria) {
+		this.em.merge(categoria);
+	}
+
+	public void remover(Categoria categoria) {
+		categoria = this.em.merge(categoria);
+		this.em.remove(categoria);
+	}
 }
--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -23,6 +23,9 @@ public class Produto {
 	private LocalDate dataCadastro = LocalDate.now();
 	@ManyToOne
 	private Categoria categoria;
+	
+	public Produto() {
+	}
 
 	public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
 		this.nome = nome;
