9951762d03002a2dd769faf3de2868b25921e7d7
rodsonnazario <rodson.nazario@gmail.com>
2022-11-12 23:32:33 -0300

    mapeamento de enum

--- src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
@@ -3,25 +3,22 @@ package br.com.alura.loja.testes;
 import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProdutos {
 
 	public static void main(String[] args) {
-		
-		Produto celular = new Produto();
-		celular.setNome("Xiaomi Redmi");
-		celular.setDescricao("Celular bom e barato");
-		celular.setPreco(new BigDecimal("800"));
-		
+
+		Produto celular = new Produto("Xiaomi Redmi", "Celular bom e barato", new BigDecimal("800"),
+				Categoria.CELULARES);
+
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao dao = new ProdutoDao(em);
-		
+
 		em.getTransaction().begin();
 		dao.cadastrar(celular);
 		em.getTransaction().commit();
