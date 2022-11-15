dd62f0cf254897ed548460964130a640dadb7771
rodsonnazario <rodson.nazario@gmail.com>
2022-11-14 21:37:33 -0300

    consulta com lazy para fazer o join só quando necessário

---
+++ src/main/java/br/com/alura/loja/testes/PerfomanceConsultas.java
@@ -0,0 +1,76 @@
+package br.com.alura.loja.testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ClienteDao;
+import br.com.alura.loja.dao.PedidoDao;
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Cliente;
+import br.com.alura.loja.modelo.ItemPedido;
+import br.com.alura.loja.modelo.Pedido;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+public class PerfomanceConsultas {
+
+	public static void main(String[] args) {
+
+		popularBancoDeDados();
+		EntityManager em = JPAUtil.getEntityManager();
+		em.getTransaction().begin();
+
+		Pedido pedido = em.find(Pedido.class, 1l);
+		System.out.println(pedido.getData());
+		System.out.println(pedido.getItens().size());
+
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	private static void popularBancoDeDados() {
+		EntityManager em = JPAUtil.getEntityManager();
+
+		Categoria celulares = new Categoria("CELULARES");
+		Categoria videogames = new Categoria("VIDEOGAMES");
+		Categoria informatica = new Categoria("INFORMATICA");
+
+		Produto celular = new Produto("Xiaomi Redmi", "Celular bom e barato", new BigDecimal("800"), celulares);
+		Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("5000"), videogames);
+		Produto macbook = new Produto("Macbook", "Macbook Pro", new BigDecimal("10000"), informatica);
+
+		Cliente cliente = new Cliente("Rodson Nazario", "12345678910");
+
+		Pedido pedido1 = new Pedido(cliente);
+		pedido1.adicionarItem(new ItemPedido(10, pedido1, celular));
+		pedido1.adicionarItem(new ItemPedido(40, pedido1, videogame));
+
+		Pedido pedido2 = new Pedido(cliente);
+		pedido2.adicionarItem(new ItemPedido(2, pedido2, macbook));
+
+		CategoriaDao categoriaDao = new CategoriaDao(em);
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		ClienteDao clienteDao = new ClienteDao(em);
+		PedidoDao pedidoDao = new PedidoDao(em);
+
+		em.getTransaction().begin();
+		categoriaDao.cadastrar(celulares);
+		categoriaDao.cadastrar(videogames);
+		categoriaDao.cadastrar(informatica);
+
+		produtoDao.cadastrar(celular);
+		produtoDao.cadastrar(videogame);
+		produtoDao.cadastrar(macbook);
+
+		clienteDao.cadastrar(cliente);
+
+		pedidoDao.cadastrar(pedido1);
+		pedidoDao.cadastrar(pedido2);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
