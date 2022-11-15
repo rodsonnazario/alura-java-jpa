9a52989d6586a579d74ad974a7b66428ecc023e4
rodsonnazario <rodson.nazario@gmail.com>
2022-11-14 23:04:24 -0300

    mapeamento de chave composta

--- src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -1,34 +1,34 @@
 package br.com.alura.loja.modelo;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import br.com.alura.loja.modelo.categoria.CategoriaId;
+
 @Entity
 @Table(name = "categorias")
 public class Categoria {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String nome;
+	@EmbeddedId
+	CategoriaId id;
 	
 	public Categoria() {
 	}
 
 	public Categoria(String nome) {
-		super();
-		this.nome = nome;
+		this.id = new CategoriaId(nome, "tipo");		
 	}
 
 	public String getNome() {
-		return nome;
+		return id.getNome();
 	}
 
 	public void setNome(String nome) {
-		this.nome = nome;
+		this.id.setNome(nome);
 	}
 
 }
