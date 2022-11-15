1b99edb15de15937da1ca0dd8124713f1b5d2ec8
rodsonnazario <rodson.nazario@gmail.com>
2022-11-14 22:25:34 -0300

    entidades embeddable

--- src/main/java/br/com/alura/loja/modelo/Cliente.java
+++ src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.modelo;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,31 +14,21 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
-	private String cpf;	
+	@Embedded
+	private DadosPessoais dadosPessoais;
 	
 	public Cliente() {
 	}
 
 	public Cliente(String nome, String cpf) {
-		super();
-		this.nome = nome;
-		this.cpf = cpf;
+		this.dadosPessoais = new DadosPessoais(nome, cpf);		
+	}	
+	
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
 	}
-
+	
 	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
+		return dadosPessoais.getNome();
 	}
 }
